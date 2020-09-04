50d58982f08c36ee6fe5ec70724da4e9c7923645
Kyle Farmer <kylefarmer85@gmail.com>
2020-09-04 18:14:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -44,6 +44,22 @@ def exit_jukebox
   puts"Goodbye"
 end
 
+# def run(songs_array)
+#   puts "Please enter a command:"
+#   input = gets.downcase.strip
+#   if input == "exit"
+#     exit_jukebox
+#   elsif input == "list"
+#     list(songs_array)
+#     run(songs_array)
+#   elsif input == "play"
+#     play(songs_array)
+#     run(songs_array)
+#   elsif input == "help"
+#     help
+#     run(songs_array)
+#   end
+# end
 
 # def run(songs_array)
 #   puts "Please enter a command:"
